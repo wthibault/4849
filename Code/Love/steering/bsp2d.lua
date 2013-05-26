7592a635a23b2d53f7e8611fb3cbf8bf2d80ea55
William Thibault <william.thibault@obscuradigital.com>
2013-05-26 08:06:10 -0700

    maze for steering fixed, new main

--- Code/Love/steering/bsp2d.lua
+++ Code/Love/steering/bsp2d.lua
@@ -16,30 +16,35 @@ BSP.OUT = {}
 
 BSP.epsilon = 0.00001
 
+function _sameVector ( a, b )
+   return a == b
+end
+
 function _splitSeg ( N, D, seg )
    local r = seg[1]
    local d = seg[2]-seg[1]
    local denom = N*d
    local frontseg = nil
    local backseg = nil
-   local dist = N*seg[1]+D
+   local dist1 = N*seg[1]+D
 
    if denom == 0 then
       -- line is parallel to the plane, figure out which side
-
-      if dist > 0 then 
+      print("_splitSeg: parallel")
+      if dist1 > 0 then 
 	 frontseg = seg 
       else
 	 backseg = seg
       end
+
    else
 
       local t = -(N*r+D)/denom
 
-      if t < 0 then 
-	 print('arg')
+      if t <= -BSP.epsilon then 
+	 print('_splitSeg: intersection behind ray tail')
 	 -- intersection is before first endpoint of seg. doesn't count.
-	 if dist > 0 then 
+	 if dist1 > 0 then 
 	    frontseg = seg 
 	 else
 	    backseg = seg
@@ -48,14 +53,56 @@ function _splitSeg ( N, D, seg )
       end
 	 
       local intpt = r + d*t
-      local dist = N*seg[2]+D  -- second point
-      if dist > 0 then
-	 frontseg = { intpt, seg[2] }
-	 backseg  = { seg[1], intpt }
-      else
+      local dist2 = N*seg[2]+D  -- second point
+
+      local inplane1 = math.abs(dist1) < BSP.epsilon
+      local inplane2 = math.abs(dist2) < BSP.epsilon
+      local infront1 = dist1 > 0 and not inplane1
+      local infront2 = dist2 > 0 and not inplane2
+      local inback1 = dist1 < 0 and not inplane1
+      local inback2 = dist2 < 0 and not inplane2
+
+      
+      if inplane1 and infront2 then
+	 frontseg = seg
+	 backseg = nil
+      elseif inplane1 and inback2 then
+	 frontseg = nil
+	 backseg = seg
+      elseif inplane2 and infront1 then
+	 frontseg = seg
+	 backseg = nil
+      elseif inplane2 and inback1 then
+	 frontseg = nil
+	 backseg = seg
+      elseif infront1 and inback2 then
 	 frontseg = { seg[1], intpt }
-	 backseg  = { intpt, seg[2] }
+	 backseg =  { intpt, seg[2] }
+      elseif inback1 and infront2 then
+	 frontseg = { intpt, seg[2] }
+	 backseg = { seg[1], intpt }
+      elseif inback1 and inback2 then
+	 frontseg = nil
+	 backseg = seg
+      elseif infront1 and infront2 then
+	 frontseg = seg
+	 backseg = nil
       end
+
+      -- if not inplane1 then
+      -- 	 if dist2 > 0 then
+      -- 	    frontseg = { intpt, seg[2] }
+      -- 	    backseg  = { seg[1], intpt }
+      -- 	 else
+      -- 	    frontseg = { seg[1], intpt }
+      -- 	    backseg  = { intpt, seg[2] }
+      -- 	 end
+      -- else
+      -- 	 if math.abs(dist) < BSP.epsilon and math.abs(dist2) < BSP.epsilon then
+      -- 	    return nil,nil
+      -- 	 end
+      -- end
+
    end
    return frontseg, backseg
 end   
@@ -152,6 +199,24 @@ function _printSeg (seg,indent)
    end
 end
 
+function BSP.printTree ( tree, indent )
+   local indent = indent or 0
+   local pad = ''
+   for i=1,indent do pad = pad .. ' ' end
+   if tree == BSP.IN then
+      print (pad,"IN")
+   elseif tree == BSP.OUT then
+      print (pad,"OUT")
+   else
+      print (pad,"N=",tree.N)
+      print (pad,"D=",tree.D)
+      print (pad,"left=")
+      BSP.printTree ( tree.left, indent + 6)
+      print (pad,"right=")
+      BSP.printTree ( tree.right, indent + 6)
+   end
+end
+
 -- line segment intersect bsp (tail should be in an "out" region,
 -- returns a vector point for the closest intersection with bsp along seg (a pair of points)
 
@@ -160,66 +225,72 @@ function BSP.segIntersect ( seg, bsp, indent )
    local nearestInt = nil
    local frontSeg, backSeg
    local indent = indent or 0
-   indent = indent + 2
-   local pad = ''
-   for i=1,indent do pad = pad .. ' ' end
+   indent = indent + 6
 
-   print ('entered segIntersect: ')
-   _printSeg(seg, indent-2)
+   if seg then 
+      local pad = ''
+      for i=1,indent do pad = pad .. ' ' end
 
-   if bsp == BSP.IN then
-      print (pad,'IN: returning ', seg[1])
-      return seg[1]
-   elseif bsp == BSP.OUT then
-      print (pad,'out: returning nil')
-      return nil
-   end
 
-   frontSeg, backSeg = _splitSeg ( bsp.N, bsp.D, seg )
+      print (pad,'entered segIntersect: ')
+      _printSeg(seg, indent-2)
 
-   local dist = bsp.N*seg[1]+bsp.D
-   if dist < 0 then
-      near = bsp.left
-      far =  bsp.right
-      nearSeg = backSeg
-      farSeg = frontSeg
-   else
-      near = bsp.right
-      far = bsp.left
-      nearSeg = frontSeg
-      farSeg = backSeg
-   end
-
-   print(pad,'after split: ')
-   _printSeg(nearSeg,indent)
-   _printSeg(farSeg,indent)
-
-   if near then
-      print (pad, 'checking nearseg ' )
-      _printSeg(nearSeg,indent)
-
-      nearestInt = BSP.segIntersect ( nearSeg, near, indent )
-      if nearestInt then
-	 print (pad,'near side intersection: ', nearestInt)
-	 return nearestInt
+      if bsp == BSP.IN then
+	 print (pad,'IN: returning ', seg[1])
+	 return seg[1]
+      elseif bsp == BSP.OUT then
+	 print (pad,'out: returning nil')
+	 return nil
       end
-   end
 
-   if far then
-      
-      print(pad, 'checking farseg')
-      _printSeg(farSeg,indent)
-      
-      nearestInt = BSP.segIntersect ( farSeg, far, indent )
-      if nearestInt then
-	 print (pad,'far side intersection: ', nearestInt)
-	 return nearestInt
+      frontSeg, backSeg = _splitSeg ( bsp.N, bsp.D, seg )
+
+      local dist = bsp.N*seg[1]+bsp.D
+      if dist < 0 then
+	 near = bsp.left
+	 far =  bsp.right
+	 nearSeg = backSeg
+	 farSeg = frontSeg
       else
-	 print (pad,'no far side intersection, returning nil')
+	 near = bsp.right
+	 far = bsp.left
+	 nearSeg = frontSeg
+	 farSeg = backSeg
+      end
+
+      print(pad,'after split: ')
+      _printSeg(nearSeg,indent)
+      _printSeg(farSeg,indent)
+
+      if near then
+	 print (pad, 'checking nearseg ' )
+	 _printSeg(nearSeg,indent)
+
+	 nearestInt = BSP.segIntersect ( nearSeg, near, indent )
+	 if nearestInt then
+	    print (pad,'near side intersection: ', nearestInt)
+	    return nearestInt
+	 end
+      end
+
+      if far then
+	 
+	 print(pad, 'checking farseg')
+	 _printSeg(farSeg,indent)
+	 
+	 nearestInt = BSP.segIntersect ( farSeg, far, indent )
+	 if nearestInt then
+	    print (pad,'far side intersection: ', nearestInt)
+	    return nearestInt
+	 else
+	    print (pad,'no far side intersection, returning nil')
+	    return nil
+	 end
+      else
+	 print (pad,'no far side intersection either, returning nil')
 	 return nil
       end
    else
-      print (pad,'no far side intersection either, returning nil')
       return nil
    end
 end
--- Code/Love/steering/main.lua
+++ Code/Love/steering/main.lua
@@ -1,344 +1,330 @@
-vector = require "vector"
-require 'middleclass'
-Stateful = require "stateful"
-bsp = require "bsp"
+camera = require ( 'camera' )
+vector = require ( 'vector' )
+gamestate = require ( 'gamestate' )
 
-numNPCs = 500
+time = 0
+
+player = {}
+player.pos = vector ( 2,2 )
+player.radius = 1
+
+goal={}
+goal.pos = vector ( 15, 15 )
+goal.radius = 1
+buzzer = love.audio.newSource("buzzer.ogg", "static")
 
 
--- This example shows using 'middleclass' objects and 'stateful.lua' to 
--- get FSMs using the Strategy pattern.
+tiles={
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,2,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,2,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,2,2,2,2,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+	  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+	  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+   }
+tiles.width = 20
+tiles.height = 20
+tiles.size = 32
+tiles.path = 2
+tiles.wall = 1
+tiles.marker = 3
+tiles.extra = 4
 
-
-
-Game = class('Game')
-Game.static.theGame = nil  -- the Singleton
-
-function Game:initialize()
-   Game.theGame = self
-   self.bullets = {}
-   self.npcs = {}
-   self.sunPosition = vector(love.graphics.getWidth()/2, 
-			love.graphics.getHeight()/2 )
-   self.gravConstant = 50.0
-   self.maxSpeed = 200
-   self.bulletTree = nil
-   self.npcTree = nil
-   self.bulletRadius = 16
-   self.npcRadius = 50
-   self.npcTimeSpentMoving = 13
-   self.npcTimeSpentStopped = 3
-   self.npcTimeSpentStoppedJitter = 3
+function tiles:indexOf (col,row) 
+      return  (row-1)*self.height + col 
 end
 
-function Game:acceleration ( pos,dt )
-   local d = self.sunPosition - pos
-   local distSquared = d:len()
-   d = d * dt
-   d = d * self.gravConstant / distSquared
-   return d
+function tiles:mapToWorld ( col, row )
+end
+------------------------------------
+--
+-- hump gamestate
+--
+menu = {}
+game = {}
+
+function menu:draw()
+    love.graphics.print("Last Elapsed Time:", 200,180)
+    love.graphics.print(time, 200,200)
+    love.graphics.print("Press Enter to continue", 10, 10)
 end
 
--------------------------------
-
-Entity = class('Entity')
-Entity.static.ents = {}
-function Entity:initialize(pos0,vel0)
-   self.pos = vector(pos0.x, pos0.y)
-   self.vel = vector(vel0.x, vel0.y)
-   -- use this pattern for ids, but never loop on indices, always use pairs not ipairs
-   -- the # op give next open slot, may be gaps due to attrition
-   self.id = #(Entity.ents)+1
-   Entity.ents[#(Entity.ents)+1] = self
-   
-end
-function Entity:update(dt)
-   -- default update uses gravity acceleration to sun
-   self.vel = self.vel + Game.theGame:acceleration ( self.pos, dt )
-   if self.vel:len() > Game.theGame.maxSpeed then
-      self.vel = self.vel / self.vel:len() * Game.theGame.maxSpeed
-   end
-   self.pos = self.pos + self.vel * dt
+function menu:keypressed(key, code)
+   print('menu:keypressed')
+    if key == 'return' then
+        gamestate.switch(game)
+    end
 end
 
-function Entity:checkFirstCollisionBruteForce(colliders)
-   -- return id of collided object (first one found) in supplied table of entities
-   -- return nil if no collision found
-   for k,ent in pairs(colliders) do
-      if ent.id ~= self.id and ent then
-	 d = ent.pos - self.pos
-	 if d:len() < (ent.radius + self.radius) then
-	    -- print("hit it", 100,50)
-	    return k
-	 end
-      end
-   end
-   return nil
-end
-function Entity:checkFirstCollision(colliderTree)
-   -- return id of collided object (first one found) in supplied table of entities
-   -- return nil if no collision found
-   local collisions = bsp.withinDisk ( colliderTree, self.pos, self.radius+Game.theGame.npcRadius )
-   if collisions then
-      for k,entpos in pairs(collisions) do
-	 ent = Game.theGame.npcs[entpos.id]
-	 if entpos ~= self.pos and ent then
-	    local d = entpos - self.pos
-	    if d:len() < (ent.radius + self.radius) then
-	       return entpos.id
-	    end
-	 end
-      end
-   end
-   return nil
-end
----------------------------------------
+--------------------------------------------------------------------------
 
-Bullet = class('Bullet', Entity)
-function Bullet:initialize ( pos, vel )
-   Entity.initialize(self, pos, vel )
-   self.radius = Game.theGame.bulletRadius
-   Game.theGame.bullets[#(Game.theGame.bullets)+1] = self
-end
-function Bullet:update(dt)
-   Entity.update(self,dt)
-end
-function Bullet:draw()
-   love.graphics.setPointSize ( self.radius * 2 )
-   love.graphics.setColor ( 255,100,100,100 )
-   love.graphics.point(self.pos.x, self.pos.y)
-end
-   
-----------------------------------------
-
-NPC = class ('NPC', Entity)
-NPC:include(Stateful)
-
-function NPC:initialize ( pos, vel )
-   Entity.initialize(self,pos,vel)
-   self.radius = Game.theGame.npcRadius
-   self.health = 100
-   Game.theGame.npcs[#(Game.theGame.npcs)+1] = self
-end
-function NPC:update(dt)
-end
-function NPC:draw()
-   love.graphics.setColor ( self.health,self.health, 255, 64 )
-   love.graphics.circle ( 'fill', self.pos.x, self.pos.y, self.radius, 32 )
-end
-function NPC:damage()
-   -- print("damage",self.health)
-   self.health = self.health - 20
-   if self.health < 0 then
-      return true
-   end
-   return false
-end
-
--- steering - avoid bullets
-function NPC:Avoid(them)
-   -- find the closest one and avoid it
-   -- return a vector for forace
-   -- uses bsp tree
-   local forceConstant = 5
-   local minDist = 100
-   local closest;
-   local d;
-   if them then
-
-      local near = bsp.withinDisk ( Game.theGame.bulletTree, self.pos, minDist )
-      if near then
-	 for k,it in pairs(near) do
-	    d = self.pos - it
-	    local dist = d:len()
-	    if dist < minDist then
-	       minDist = dist
-	       closest = it
-	    end
-	 end
-      end
-
-      if closest then
-        local steeringForce = (self.pos - closest) * 
-              forceConstant / minDist*minDist
-        return steeringForce
-      end
-   end
-   return nil
-end
-
--- steering - avoid bullets
-function NPC:AvoidBruteForce(them)
-   -- find the closest one and avoid it
-   -- return a vector for forace
-   -- XXX needs spatial index
-   local forceConstant = 5
-   local minDist = 100
-   local closest;
-   local d;
-   if them then
-      for k,it in pairs(them) do
-        d = self.pos - it.pos
-        local dist = d:len()
-        if dist < minDist then
-          minDist = dist
-          closest = it
-        end
-      end
-      if closest then
-        local steeringForce = (self.pos - closest.pos) * 
-              forceConstant / minDist*minDist
-        return steeringForce
-      end
-   end
-   return nil
-end
-   
---- NPC states --- 
--- With stateful classes, you can override a method that belongs to the stateful class
--- by defining a method of the same name in the state, and passing the State to the addState method.
--- States also get enteredState() and exitedState() callbacks when the state changes.
-
--- The Idle state draws itself as a large stationary disk.
--- If it gets hit, or too much time elapses, it changes state to Moving
-Idle = NPC:addState('Idle')
-function Idle:enteredState()
-   self.radius = Game.theGame.npcRadius
-   self.startTime = love.timer.getTime()
-   self.timeout = Game.theGame.npcTimeSpentStopped + math.random() * Game.theGame.npcTimeSpentStoppedJitter
---   self.sound = love.audio.newSource("bing.wav", "static") 
---   love.audio.play(self.sound)
-   self.vel = vector(0,0)
-end
-function Idle:update(dt)
-   if self.health < 100 or (love.timer.getTime() - self.startTime) > self.timeout then
-      self:gotoState('Moving')
-   end
-end
-function Idle:exitedState()
---   love.audio.stop(self.sound)
-end
-
--- The Moving state changes its radius and starts a timer to revert to Idle in 10 seconds
--- Moving also overrides the draw method by changing the radius,  
--- calling the old "owner's" draw method and then drawing a 'tail'
--- we also apply a steering force to our velocity before the entity update
-Moving = NPC:addState('Moving')
-function Moving:enteredState()
-   self.radius = 8
-   self.startTime = love.timer.getTime()
-   self.revertTime = Game.theGame.npcTimeSpentMoving
---   self.sound = love.audio.newSource("upsy.wav", "static") 
---   love.audio.play(self.sound)
-end
-function Moving:update(dt)
-   elapsedTime = love.timer.getTime() - self.startTime -- time since we entered the state
-   self.radius =  4+4 *math.abs(math.sin(9*math.pi * elapsedTime)) -- 9Hz flutter
-   local steeringForce = self:Avoid ( Game.theGame.bullets )
-   if steeringForce then
-      self.vel = self.vel + steeringForce * dt
-   end
-   Entity.update(self,dt)
-   if elapsedTime > self.revertTime then
-      self.health = 100
-      self:gotoState('Idle')
-   end
-end
-function Moving:draw()
-   NPC.draw ( self )
-   local ahead = self.pos - self.vel * 0.15
-   love.graphics.line ( self.pos.x, self.pos.y, ahead.x, ahead.y )
-end
-function Moving:exitedState()
---   love.audio.stop(self.sound)
-end
-
-----------------------------------------
-
-
-function love.load()
-   if arg[#arg] == "-debug" then require("mobdebug").start() end 
-   
-   require 'test-bsp2d'
-   
-   Game.theGame = Game:new()
---   love.graphics.setBackgroundColor(54, 172, 248,255)
-   love.graphics.setBackgroundColor(0,0,0,255)
-   local w = love.graphics.getWidth()
-   local h = love.graphics.getHeight()
---   love.graphics.setMode(w,h,true)
-   for i=1,numNPCs do
-      local target = NPC:new ( vector(w/2,i * h/numNPCs) , vector(100,0) )
-      target:gotoState('Idle')
-   end
-   -- for demo, start with a bullet or more
+function love.load() 
+   if arg[#arg] == "-debug" then require("mobdebug").start() end
    math.randomseed(os.time())
-   xSpacing = math.random() * 20.0
-   ySpacing = math.random() * 40.0
-   for i=1,numNPCs*2 do
-      local b = Bullet:new(vector( xSpacing*i,ySpacing*i ), vector (100,0) )
+   makeTiles()
+   tiles:makeRandomMaze()
+   cam = camera.new(100,100,1,0)
+   -- cam.pos = player.pos * tiles.size + vector(tiles.size/2, tiles.size/2)
+   -- cam:lookAt ( cam.pos.x, cam.pos.y )
+   gamestate.registerEvents()
+   gamestate.switch(menu)
+end
+
+function updateCam ( x, y, dt )
+    local offset = vector (x,y) * dt
+    cam.pos = cam.pos + offset
+    cam:lookAt ( cam.pos.x, cam.pos.y )
+  end
+
+function game:enter()
+   tiles:makeRandomMaze()
+  player.pos = vector(2,2)
+  local findThePath = function (pos) 
+     while tiles:getTile(pos:unpack()) == tiles.wall do 
+	pos = pos + vector(math.random(-1,1), math.random(-1,1)) end
+     return pos
+  end
+  player.pos = findThePath(player.pos)
+  goal.pos = findThePath(goal.pos)
+  cam.pos = player.pos * tiles.size + vector(tiles.size/2, tiles.size/2)
+  time = 0
+end
+
+function game:update(dt)
+   time = time + dt
+   player.radius = tiles.size/2 * math.sin ( 4 * time )
+   goal.radius = tiles.size/2 * math.sin ( 10 * time )
+   local speed = 100
+   if love.keyboard.isDown('right') then updateCam ( speed,0, dt ) end
+   if love.keyboard.isDown('left') then updateCam ( -speed,0, dt ) end
+   if love.keyboard.isDown('up') then updateCam ( 0,-speed, dt ) end
+   if love.keyboard.isDown('down') then updateCam ( 0,speed, dt ) end
+end
+
+function game:draw()
+   cam:lookAt ( cam.pos:unpack() )
+   cam:attach()
+   drawWorld()
+   cam:detach()
+   drawHud()
+end
+
+function isLegalTile ( x, y )
+  return tiles:getTile(x,y) ~= tiles.wall
+end
+
+function game:keypressed(key)
+   local pos = player.pos
+   local offset 
+   if key == "d" then offset = vector(1,0) end
+   if key == "a" then offset = vector(-1,0) end
+   if key == "w" then offset = vector (0,-1) end
+   if key == "s" then offset = vector ( 0,1 ) end
+   if key == "escape" then love.event.push('quit') end
+
+   if offset then
+      pos = pos + offset
+      if isLegalTile ( pos:unpack() ) then
+	 player.pos = pos
+	 cam.pos = pos * tiles.size
+      else love.audio.play(buzzer)
+      end
+ 
+      if player.pos == goal.pos then
+	 gamestate.switch(menu)
+      end
    end
 end
 
-function love.mousepressed(x,y,button)
-   local b = Bullet:new(vector(x,y), vector(100,0))
+
+------------------------------------------------
+
+------------------------------------------------
+
+function drawWorld()
+   drawBackground()
+   drawNPCs()
+   drawPlayer()
 end
 
+function drawHud()
+   love.graphics.setColor(255,255,255)
+   love.graphics.printf ("Camera Cam", 25, 25, 800, "center")
+end
 
-function love.draw()
-   love.graphics.setColorMode('replace')
-   love.graphics.setBlendMode('alpha')
-
-   love.graphics.setColor ( 255,255,0,255 )
-   love.graphics.circle ( 'fill', Game.theGame.sunPosition.x, Game.theGame.sunPosition.y, 10 )
-
---   for i,ent in ipairs(Entity.ents) do
---      ent:draw()
---   end
-   for i,ent in pairs(Game.theGame.npcs) do
-      ent:draw()
-   end
-   for i,ent in pairs(Game.theGame.bullets) do
-      ent:draw()
+function drawBackground()
+   for row = 1,tiles.height do
+      for col = 1, tiles.width do
+        drawTile ( row, col )
+      end
    end
 end
 
-
-function love.update(dt)
+function drawTile( row, col )
+   local i = tiles:indexOf ( col,row ) -- (row-1)*tiles.height + col
+   local x = col * tiles.size
+   local y = row * tiles.size
+   --[[if tiles[i] == 1 then
+     love.graphics.setColor ( 255,0,0 )
+   end
+   if tiles[i] == 2 then
+    love.graphics.setColor ( 0,0,255 )
+   end
+   love.graphics.rectangle ( 'fill', x, y, tiles.size, tiles.size )
+   ]]--
    
-   function buildFromPos(t)
-      local pts={}
-      for i,b in pairs(t) do
-	 if b then 
-	    b.pos.id = i  -- attach the array index (ent or bullet id)
-	    table.insert(pts,b.pos) 
+   love.graphics.drawq ( atlas, quads[tiles[i]], x, y )
+end
+
+function drawNPCs()
+  local pos = goal.pos * tiles.size + vector(tiles.size/2, tiles.size/2)
+  love.graphics.setColor(255,0,0)
+  love.graphics.circle('fill', pos.x, pos.y, goal.radius, 16)
+end
+
+function drawPlayer()
+  local pos = player.pos * tiles.size + vector(tiles.size/2, tiles.size/2)
+  love.graphics.setColor(0,255,0)
+  love.graphics.circle('fill', pos.x, pos.y, player.radius, 16)
+end
+
+--------------------------------------------------
+
+function makeTiles()
+   -- make quads
+   atlas = love.graphics.newImage('64X64.png')
+   quads = { love.graphics.newQuad ( 0,0,    32,32, 64,64 ), 
+             love.graphics.newQuad ( 0, 32,  32,32, 64, 64 ),
+             love.graphics.newQuad ( 32, 0,  32,32, 64, 64 ),
+             love.graphics.newQuad ( 32, 32, 32,32, 64, 64 )
+      }
+   
+end
+
+
+function tiles:setTile ( x, y, id )
+   if id then
+      self[self:indexOf(x,y)] = id
+   else
+      print('added nil to tile ',x,y)
+   end
+end
+
+function tiles:getTile ( x, y )
+   return self[self:indexOf(x,y)]
+end
+
+function tiles:makeRandomMaze()
+   -- since walls are needed, we'll look for unvisted neighbors that are TWO rows/cols
+   --   away, and then mark it and the one between it and the current point as "path" tiles.
+
+   -- recursive subdivision
+   -- clear map, set all as path tiles
+   for x = 1,self.width do
+      for y = 1, self.height do
+	 if x == 1 or x == self.width or y == 1 or y == self.height then
+	    self:setTile( x, y, self.wall )
+	 else
+	    self:setTile ( x, y, self.path )
 	 end
       end
-      --print('building bsp tree with', #pts, 'points')
-      return bsp.build(pts)
    end
-    -- build bsp trees for bullets
-    Game.theGame.bulletTree = buildFromPos(Game.theGame.bullets)
-
-   -- update the ents
-   for i,ent in pairs(Entity.ents) do
-      ent:update(dt)
-   end
-  -- build bsp tree for updated ents
-  Game.theGame.npcTree = buildFromPos(Game.theGame.npcs)
-  
-   -- collide bullets with npcs
-   for i,bullet in pairs(Game.theGame.bullets) do
---      local hit = bullet:checkFirstCollision(Game.theGame.npcs)
-      local hit = bullet:checkFirstCollision(Game.theGame.npcTree)
-      if hit and Game.theGame.npcs[hit] then
-        if Game.theGame.npcs[hit]:damage() then
-          Game.theGame.npcs[hit] = nil
-        end
-        -- delete bullet
-        Game.theGame.bullets[i] = nil 
-      end
-   end
-
+--   tiles:buildMaze ( 1, self.width, 1, self.height )
+   tiles:buildMaze ( 2, self.width-1, 2, self.height-1 )
 end
 
+function tiles:goodWallPosition ( wallPos, axis, minx, maxx, miny, maxy )
+   local minCorner = vector ( minx, miny )
+   local maxCorner = vector ( maxx, maxy )
+   local otheraxis = axis=='x' and 'y' or 'x'
+   local P0 = vector ( 0,0 )
+   local P1 = vector ( 0,0 )
+   P0[axis] = wallPos
+   P0[otheraxis] = minCorner[otheraxis]-1
+   P1[axis] = wallPos
+   P1[otheraxis] = maxCorner[otheraxis]+1
+
+
+   local tile0 = self:getTile(P0:unpack())
+   local tile1 = self:getTile(P1:unpack())
+   local isWall0 = tile0==self.wall
+   local isWall1 = tile1==self.wall
+
+   -- debug
+   self:setTile(P0.x, P0.y, self.extra)
+   self:setTile(P1.x, P1.y, self.extra)
+
+   return isWall0 and isWall1
+end
+
+function tiles:buildMaze ( minx, maxx, miny, maxy )
+   local dx = maxx - minx
+   local dy = maxy - miny
+   local axis
+   local minSize = 4
+   if ( dx <= minSize or dy <= minSize ) then return end
+   -- pick a random split point
+   local mi
+   local ma
+   if dx > dy then
+      mi = minx
+      ma = maxx
+      axis = 'x'
+   else
+      mi = miny
+      ma = maxy
+      axis = 'y'
+   end
+
+--   local div = math.random ( mi+2, ma-2 )
+
+   local div
+   local tries = 0
+   repeat
+      div = math.random ( mi+2, ma-2 )
+      tries = tries + 1
+   until tiles:goodWallPosition ( div, axis, minx, maxx, miny, maxy ) or tries > 100
+
+   if tries > 100 then print ("100 tries!") return end
+
+   -- set them as walls
+   --  and pick a random pos along wall and set as path
+   if dx > dy then
+      x = div
+      for y = miny,maxy do
+	 tiles:setTile(x,y, self.wall)
+      end
+      -- put a door at a random spot along the wall
+      local doorPos = math.random(miny+1, maxy-1)
+
+      tiles:setTile(x, doorPos, self.marker)
+      
+      -- recurse on each side
+      tiles:buildMaze ( minx, div-1, miny, maxy )
+      tiles:buildMaze ( div+1, maxx, miny, maxy )
+   else
+      y = div
+      for x = minx,maxx do
+	 tiles:setTile(x,y,self.wall)
+      end
+      local doorPos = math.random(minx+1, maxx-1)
+      tiles:setTile(doorPos, y, self.marker)
+      -- recurse on each side
+      tiles:buildMaze ( minx, maxx, miny, div-1 )
+      tiles:buildMaze ( minx, maxx, div+1, maxy )
+   end
+end
