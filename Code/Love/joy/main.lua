6ebe835b64b90612b1ab1e299e7ac9a39353ed78
William Thibault <william.thibault@gmail.com>
2015-03-31 07:43:02 -0700

    added joy, from wiki

---
+++ Code/Love/joy/main.lua
@@ -0,0 +1,29 @@
+-- from the love2d wiki: https://www.love2d.org/wiki/Joystick
+
+function love.load()
+   local joysticks = love.joystick.getJoysticks()
+   joystick = joysticks[1]
+
+   position = {x = 400, y = 300}
+   speed = 300
+end
+
+function love.update(dt)
+    if not joystick then return end
+
+    if joystick:isGamepadDown("dpleft") then
+        position.x = position.x - speed * dt
+    elseif joystick:isGamepadDown("dpright") then
+        position.x = position.x + speed * dt
+    end
+
+    if joystick:isGamepadDown("dpup") then
+        position.y = position.y - speed * dt
+    elseif joystick:isGamepadDown("dpdown") then
+        position.y = position.y + speed * dt
+    end
+end
+
+function love.draw()
+    love.graphics.circle("fill", position.x, position.y, 50)
+end
