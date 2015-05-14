a6681ea021aa6ef349f3ebf0d61b14cf36c57b02
William Thibault <william.thibault@gmail.com>
2015-05-14 11:46:15 -0700

    added spritesheets example

---
+++ Code/Love/spritesheets/config.lua
@@ -0,0 +1,12 @@
+local config = {}
+-- explosion from http://xboxforums.create.msdn.com/forums/t/32476.aspx
+config.explodeSheetFilename = 'explosion17.png'
+
+-- walk cycle from https://mattrobenolt.com/jquery-sprite-plugin/walk-cycle.png
+config.walkSheetFilename = 'walk-cycle.png'
+config.spriteWidth = 48
+config.spriteHeight = 92
+
+config.margin = 100
+
+return config
---
+++ Code/Love/spritesheets/main.lua
@@ -0,0 +1,73 @@
+local anim8 = require 'anim8'
+local vector = require 'vector'
+local config = require 'config'
+
+local explodeSpriteSheet,explodeAnimation
+local runcycleSpriteSheet,runcycleAnimation
+
+function explosionDone(self,loops)
+   self:pauseAtEnd()
+   exploding = false
+   playerAlive = false
+end
+
+function love.load()
+   xmin = config.margin
+   ymin = config.margin
+   xmax = love.window.getWidth() - config.margin
+   ymax = love.window.getHeight() - config.margin
+   
+   explodeSpriteSheet = love.graphics.newImage(config.explodeSheetFilename)
+   local g = anim8.newGrid(64,64, 
+			   explodeSpriteSheet:getWidth(), 
+			   explodeSpriteSheet:getHeight())
+   explodeAnimation = anim8.newAnimation( g('1-5','1-5'), 0.05,
+					  explosionDone )
+   explodeAnimation:pauseAtStart()
+
+   runcycleSpriteSheet = love.graphics.newImage(config.walkSheetFilename)
+   local rg = anim8.newGrid(config.spriteWidth, config.spriteHeight,
+			    runcycleSpriteSheet:getWidth(),
+			    runcycleSpriteSheet:getHeight())
+   runcycleAnimation = anim8.newAnimation ( rg('1-9','7-7'), 0.1 )
+   
+   manstart = vector.new( (xmax+xmin)/2, (ymax+ymin)/2 )
+   manpos = manstart:clone()
+
+   exploding = false
+   playerAlive = true
+
+   love.graphics.setBackgroundColor ( 255,255,255 )
+
+
+end
+
+function love.update(dt)
+   runcycleAnimation:update(dt)
+   if manpos.x >= xmax-config.spriteWidth then
+      runcycleAnimation:pause()
+      if not exploding then
+	 exploding = true
+	 explodeAnimation:resume()
+      end
+   else
+      manpos = manpos + vector.new(1,0)
+   end
+
+   if exploding then
+      explodeAnimation:update(dt)
+   end
+end
+
+function love.draw()
+   love.graphics.setColor ( 255,0,0 )
+   love.graphics.rectangle ( 'line', xmin, ymin, xmax-xmin, ymax-ymin )
+
+   love.graphics.setColor ( 255,255,255 )
+   if playerAlive then
+      runcycleAnimation:draw(runcycleSpriteSheet, manpos:unpack())
+   end
+   if exploding then
+      explodeAnimation:draw (explodeSpriteSheet, manpos:unpack())
+   end
+end
